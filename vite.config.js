410487bbb1d5821563a6a6f281db1e973c959804
SIRO ARC <dominikschaeffler@googlemail.com>
2026-07-25 20:00:27 +0200

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/SIRO-ARC-Statistics/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
